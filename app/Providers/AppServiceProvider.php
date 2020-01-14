5fab871fa90042160a4a3ed923b1d3b151f198f5
Admin <C:\Users\User\AppData\Roaming\The Bat!>
2020-01-14 22:56:07 +0300

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Dusk\DuskServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Schema\Builder;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Builder::defaultStringLength(191);
+
+        if ($this->app->runningUnitTests()) {
+            Schema::defaultStringLength(191);
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->environment('local', 'testing')) {
+            $this->app->register(DuskServiceProvider::class);
+        }
+    }
+}
